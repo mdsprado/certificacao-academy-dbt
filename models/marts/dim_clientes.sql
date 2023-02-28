bdaa4b76c48b7f039fdc7126f0257b658e85bb40
Maria do Socorro <mariaprado.01@outlook.com>
2023-02-28 13:52:50 +0000

    corrigido tabela cliente

--- models/marts/dim_clientes.sql
+++ models/marts/dim_clientes.sql
@@ -1,42 +1,20 @@
  with 
     clientes as (
-        select *
+        select 
+            id_cliente 
+            , cast((primeiro_nome_cliente || ' ' || ultimo_nome_cliente) as STRING) as nome_completo_cliente
+            , empresa_cliente
+            , endereco_cliente
+            , cidade_cliente
+            , estado_cliente
+            , pais_cliente
+            , cep_cliente
+            , telefone_cliente
+            , fax_cliente
+            , email_cliente
         from {{ ref('stg_mds__clientes')}}
     )
 
-    , entidade as (
-        select *
-        from {{ ref('stg_mds__entidade_comercial')}}
-    )
-    , pessoas as (
-        select *
-        from {{ ref('stg_mds__pessoas')}}
-    )
-
-, juncao_tabelas1 as (
-    select 
-        clientes.id_cliente
-        , clientes.id_pessoa
-        , clientes.id_loja
-        , clientes.id_territorio
-        , entidade.id_entidade
-        
-    from clientes
-    left join entidade on clientes.id_pessoa = entidade.id_pessoa
-    
-)
-, juncao_tabelas2 as (
-    select 
-        juncao_tabelas1.id_cliente
-        , juncao_tabelas1.id_pessoa
-        , juncao_tabelas1.id_loja
-        , juncao_tabelas1.id_territorio
-        , pessoas.id_entidade
-        , pessoas.tipo_pessoa
-        , pessoas.nome_completo
-    from juncao_tabelas1
-    left join pessoas on juncao_tabelas1.id_entidade = pessoas.id_entidade
-)
 
 
 
@@ -44,7 +22,7 @@
     select
         row_number() over (order by id_cliente) as pk_cliente
         , *
-    from juncao_tabelas2
+    from clientes
 )
 
 select *
--- models/staging/stg_mds__clientes.sql
+++ models/staging/stg_mds__clientes.sql
@@ -2,12 +2,19 @@ with
     source_clientes as (
         select
              
-            cast(customerid AS INTEGER) as id_cliente
-            , cast(personid as INTEGER) as id_pessoa
-            , cast(storeid as INTEGER) as id_loja
-            , cast(territoryid as INTEGER) as id_territorio
-            --, rowguid
-            --, modifieddate
+            cast(CustomerId as int) as id_cliente 
+            , cast(FirstName as string) as primeiro_nome_cliente
+            , cast(LastName as string)  as ultimo_nome_cliente
+            , cast(Company as string)  as empresa_cliente
+            , cast(Address as string) as endereco_cliente
+            , cast(City as string) as cidade_cliente
+            , cast(State as string) as estado_cliente
+            , cast(Country as string) as pais_cliente
+            , cast(PostalCode as string) as cep_cliente
+            , cast(Phone as string) as telefone_cliente
+            , cast(Fax as string) as fax_cliente
+            , cast(Email as string) as email_cliente
+            --, SupportRepId
                             
         from {{ source ('mds','customer')}} -- a macro source esta pegando no banco
     )
